55ea1d0ca35308ce598e0779e8077f4acb5fcb9b
zeynepunluer <143020599+zeynepunluer@users.noreply.github.com>
2024-01-01 15:38:31 +0300

    city card

---
+++ app/src/main/java/com/example/citycard/BalanceCheck.java
@@ -0,0 +1,14 @@
+package com.example.citycard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BalanceCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance_check);
+    }
+}
