fc4ca9c2295d9446ed026a508dcbc7bfa01b6d7a
zeynepunluer <143020599+zeynepunluer@users.noreply.github.com>
2024-01-07 23:29:45 +0300

    fixed"

--- app/src/main/java/com/example/citycard/HomeScreen.java
+++ app/src/main/java/com/example/citycard/HomeScreen.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -51,6 +53,9 @@ public class HomeScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
 
 
     }
