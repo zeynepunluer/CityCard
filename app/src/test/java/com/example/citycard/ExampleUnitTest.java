841cececcbab3e2bec374ab2d31711fc80f2a016
zeynepunluer <143020599+zeynepunluer@users.noreply.github.com>
2024-01-17 01:31:25 +0300

    edit

--- app/src/test/java/com/example/citycard/ExampleUnitTest.java
+++ app/src/test/java/com/example/citycard/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +16,6 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+   
 }
